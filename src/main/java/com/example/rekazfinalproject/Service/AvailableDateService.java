3a6beb516ad73a45dc9c34941cc2a7a199d15e48
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-09-14 23:24:59 +0300

    Update AvailableDateService.java

--- src/main/java/com/example/rekazfinalproject/Service/AvailableDateService.java
+++ src/main/java/com/example/rekazfinalproject/Service/AvailableDateService.java
@@ -1,7 +1,6 @@
 package com.example.rekazfinalproject.Service;
 
 
-
 import com.example.rekazfinalproject.Api.ApiException;
 import com.example.rekazfinalproject.Model.AvailableDate;
 import com.example.rekazfinalproject.Model.Investor;
@@ -31,7 +30,7 @@ public class AvailableDateService {
             throw new ApiException("Investor not found");
         }
         for(AvailableDate availableDate1 : availableDateRepository.findAll()){
-            if(availableDate1.getInvestor().getId()==investorId){
+            if(availableDate1.getInvestor().getId()==investorId && availableDate1.getDate().equals(availableDate.getDate())  ){
                 throw new ApiException("this date already added");
             }
         }
@@ -50,11 +49,36 @@ public class AvailableDateService {
                 investorAvailableDates.add(availableDate);
             }
         }
-        if(investorAvailableDates.size()==0){
+        if(investorAvailableDates.isEmpty()){
             throw new ApiException("Investor doesn't have any available dates");
         }
         return investorAvailableDates;
 
+    }
 
+    public void updateAvailableDate(int id , AvailableDate availableDate) {
+        AvailableDate availableDate1 = availableDateRepository.findAvailableDateById(id);
+        if(availableDate1==null){
+            throw new ApiException("Investor not found");
+        }
+        availableDate1.setDate(availableDate.getDate());
+        availableDate1.setBooked(availableDate.isBooked());
+        availableDateRepository.save(availableDate1);
+    }
+
+    public void deleteAvailableDate(int id) {
+        AvailableDate availableDate = availableDateRepository.findAvailableDateById(id);
+        if(availableDate==null){
+            throw new ApiException("Investor not found");
+        }
+        if(availableDate.isBooked()){
+            throw new ApiException("Booked date can't be deleted");
+        }
+
+
+        availableDateRepository.delete(availableDate);
     }
 }
+
+
+
