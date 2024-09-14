b21bbfc0102213f79df2418f37a33335066486a0
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-09-14 22:46:31 +0300

    Update ComplaintService.java

--- src/main/java/com/example/rekazfinalproject/Service/ComplaintService.java
+++ src/main/java/com/example/rekazfinalproject/Service/ComplaintService.java
@@ -11,17 +11,47 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ComplaintService {
-
-    private final ComplaintRepository complaintRepository;
-
+private final ComplaintRepository complaintRepository;
+    private final InvestorRepository investorRepository;
+    private final OwnerRepository ownerRepository;
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
     public List<Complaint> getAllComplaint() {
         return complaintRepository.findAll();
     }
-
-    public void addNewComplaint(Complaint complaint) {
+    //Shahad
+    //investor send complaint
+    public void investorSendComplaint(Integer investorId,Complaint complaint) {
+        Investor investor=investorRepository.findInvestorById(investorId);
+        if(investor==null){
+            throw new ApiException("Investor not found");
+        }
+        Project project=projectRepository.findProjectByInvestorId(investorId);
+        if(!project.getId().equals(complaint.getProject_num())){
+            throw new ApiException("You have no right to complain.");
+        }
+        complaint.setInvestor(investor);
+        complaint.setOwner(project.getOwner());
+        complaint.setStatus(Complaint.ComplaintStatus.UNDER_REVIEW);
         complaintRepository.save(complaint);
     }
-
+    //Shahad
+         //Owner send complaint
+        public void ownerSendComplaint(Integer ownerId,Complaint complaint) {
+        Owner owner=ownerRepository.findOwnerById(ownerId);
+        if(owner==null){
+            throw new ApiException("Owner not found");
+        }
+            Project project=projectRepository.findProjectByOwnerId(ownerId);
+        if(!project.getId().equals(complaint.getProject_num())){
+           throw new ApiException("You have no right to complain.");
+        }
+        complaint.setOwner(owner);
+        complaint.setInvestor(project.getInvestor());
+        complaint.setStatus(Complaint.ComplaintStatus.UNDER_REVIEW);
+        complaintRepository.save(complaint);
+    }
+    //Shahad
     public void updateComplaint(Integer id,Complaint complaint) {
         Complaint complaint1 = complaintRepository.findComplaintsById(id);
         if (complaint1 == null) {
@@ -35,7 +65,8 @@ public class ComplaintService {
 
     }
 
-
+    //Shahad
+    //admin deleted
     public void deleteComplaint(Integer id) {
         Complaint complaint1 = complaintRepository.findComplaintsById(id);
 
@@ -43,5 +74,39 @@ public class ComplaintService {
             throw new ApiException("Complaint not found");
         }
         complaintRepository.delete(complaint1);
+    }
+    //shahad
+    //complaint status to IN_PROGRESS by admin
+    public void startProcessingComplaint(Integer adminId,Integer complaintId) {
+        User user=userRepository.findUserById(adminId);
+        if(user==null||!user.getRole().equalsIgnoreCase("ADMIN")||user.isActive()==false){
+            throw new ApiException("Admin not found");
+        }
+        Complaint complaint = complaintRepository.findComplaintsById(complaintId);
+        if (complaint == null) {
+            throw new ApiException("Complaint not found");
+        }
+        if (complaint.getStatus() != Complaint.ComplaintStatus.UNDER_REVIEW) {
+            throw new ApiException("Complaint is not in review state");
+        }
+        complaint.setStatus(Complaint.ComplaintStatus.IN_PROGRESS);
+        complaintRepository.save(complaint);
+    }
+    //shahad
+    //complaint status to RESOLVED by admin
+    public void resolveComplaint(Integer adminId,Integer complaintId) {
+        User user=userRepository.findUserById(adminId);
+        if(user==null||!user.getRole().equalsIgnoreCase("ADMIN")||user.isActive()==false){
+            throw new ApiException("Admin not found");
+        }
+        Complaint complaint = complaintRepository.findComplaintsById(complaintId);
+        if (complaint == null) {
+            throw new ApiException("Complaint not found");
+        }
+        if (complaint.getStatus() != Complaint.ComplaintStatus.IN_PROGRESS) {
+            throw new ApiException("Complaint is not in progress");
+        }
+        complaint.setStatus(Complaint.ComplaintStatus.RESOLVED);
+        complaintRepository.save(complaint);
     }
 }
