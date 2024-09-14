2db8384b60a774c6eb87e4930dac13b57b5f7d1d
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-09-14 23:12:04 +0300

    Update Subscription.java

--- src/main/java/com/example/rekazfinalproject/Model/Subscription.java
+++ src/main/java/com/example/rekazfinalproject/Model/Subscription.java
@@ -27,7 +27,6 @@ public class Subscription {
     @Column(columnDefinition = "int not null")
     private int duration ;
 
-    @Pattern(regexp = "^(Standard|Premium)$")
     @Column(columnDefinition = "varchar(20) not null")
     private String type ;
 
