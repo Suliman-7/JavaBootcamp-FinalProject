e506f85633697d39011b5dc8f67e9e392b54cd4b
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-09-14 22:37:41 +0300

    Update RatingRepository.java

--- src/main/java/com/example/rekazfinalproject/Repository/RatingRepository.java
+++ src/main/java/com/example/rekazfinalproject/Repository/RatingRepository.java
@@ -4,6 +4,9 @@ import com.example.rekazfinalproject.Model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
-    Rating findRatingById(Integer id);
+   Rating findRatingById(Integer id);
+
+    @Query("SELECT AVG(r.score) FROM Rating r WHERE r.investor.id=?1")
+    Double findAverageRatingByInvestorId( Integer investorId);
 
 }
