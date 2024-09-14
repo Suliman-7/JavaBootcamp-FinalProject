eb0a52ae351d3426fce07822dbbee76b20dc260c
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-09-14 23:15:28 +0300

    Add files via upload

---
+++ src/main/java/com/example/rekazfinalproject/Model/Question.java
@@ -0,0 +1,38 @@
+package com.example.rekazfinalproject.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "Question should not be null")
+    @Column(columnDefinition = "VARCHAR(500) NOT NULL")
+    private String question;
+
+    @Column(columnDefinition = "VARCHAR(500)")
+    private String answer;
+
+    @ManyToOne
+    @JsonIgnore
+    private Investor investor;
+
+    @ManyToOne
+    @JsonIgnore
+    private Owner owner;
+
+
+}
