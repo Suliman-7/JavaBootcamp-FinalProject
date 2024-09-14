ced0d1411a6ba0ba144b24e58620d66e62f1b43f
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-09-14 23:18:33 +0300

    Update ConsultationRepository.java

--- src/main/java/com/example/rekazfinalproject/Repository/ConsultationRepository.java
+++ src/main/java/com/example/rekazfinalproject/Repository/ConsultationRepository.java
@@ -2,8 +2,14 @@ package com.example.rekazfinalproject.Repository;
 
 import com.example.rekazfinalproject.Model.Consultation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultationRepository extends JpaRepository<Consultation, Integer> {
     Consultation findConsultationById(Integer id);
+
+    @Query("SELECT CASE WHEN DATEDIFF(c.consultationDate, :currentDate) > 0 THEN true ELSE false END " +
+            "FROM Consultation c " +
+            "WHERE c.id = :id AND c.status <> 'Canceled'")
+    boolean canCancelBeforeConsultationDate( Integer id,  LocalDate currentDate);
+
+    List<Consultation> findConsultationByOwnerId(Integer ownerId);
+    List<Consultation> findConsultationByInvestorId(Integer investorId);
 
 }
