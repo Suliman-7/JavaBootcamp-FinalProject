5f75832391f3bd5dd0aad55f1443dbb77c6b3218
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-09-14 23:21:45 +0300

    Update ConsultationController.java

--- src/main/java/com/example/rekazfinalproject/Controller/ConsultationController.java
+++ src/main/java/com/example/rekazfinalproject/Controller/ConsultationController.java
@@ -14,14 +14,15 @@ import org.springframework.web.bind.annotation.*;
 public class ConsultationController {
     private final ConsultationService consultationService;
 
-    @GetMapping("/get")
-    public ResponseEntity getAllConsultations(){
-        return ResponseEntity.status(200).body(consultationService.getAllConsultations());
+    
+    @GetMapping("/get/{id}")
+    public ResponseEntity getAllConsultations(@PathVariable Integer id){
+        return ResponseEntity.status(200).body(consultationService.getAllConsultations(id));
     }
 
-    @PostMapping("/add")
-    public ResponseEntity addConsultation(@Valid @RequestBody Consultation consultation){
-        consultationService.addNewConsultation(consultation);
+    @PostMapping("/book-consultation/{ownerId}/{investorId}")
+    public ResponseEntity addConsultation(@PathVariable Integer ownerId,@PathVariable Integer investorId,@Valid @RequestBody Consultation consultation){
+        consultationService.bookConsultation(ownerId,investorId,consultation);
         return ResponseEntity.status(200).body(new ApiResponse("Consultation successfully added"));
     }
 
@@ -36,5 +37,33 @@ public class ConsultationController {
         consultationService.deleteConsultation(id);
         return ResponseEntity.status(200).body(new ApiResponse("Consultation successfully deleted"));
     }
+    @PutMapping("/extend-duration/{ownerId}/{conId}/{duration}")
+    public ResponseEntity extendConsultationDuration(@PathVariable Integer ownerId,@PathVariable Integer conId,@PathVariable double duration){
+        consultationService.extendConsultationDuration(ownerId,conId,duration);
+        return ResponseEntity.status(200).body(new ApiResponse("The duration has been updated successfully."));
+    }
+    @PutMapping("/canceled/owner-id/{ownerid}/con-id/{conid}")
+    public ResponseEntity ownerCanceledConsultation(@PathVariable Integer ownerid,@PathVariable Integer conid){
+        consultationService.ownerCanceledConsultation(ownerid,conid);
+        return ResponseEntity.status(200).body(new ApiResponse("Owner canceled consultation "));
+    }
+    @PutMapping("/canceled/investor-id/{investorid}/con-id/{conid}")
+    public ResponseEntity  InvestorCanceledConsultation(@PathVariable Integer investorid,@PathVariable Integer conid){
+        consultationService.investorCanceledConsultation(investorid,conid);
+        return ResponseEntity.status(200).body(new ApiResponse("Investor canceled consultation"));
+    }
 
+    @GetMapping("/get-available-consultation/{owner}/{investor}")
+    public ResponseEntity getAvailableConsultationDates(@PathVariable Integer owner,@PathVariable Integer investor){
+        return ResponseEntity.status(200).body(consultationService.getAvailableConsultationDatesForInvestor(owner,investor));
+    }
+    @GetMapping("/get-owner-consultation/{owner}")
+    public ResponseEntity listConsultationForOwner(@PathVariable Integer owner){
+        return ResponseEntity.status(200).body(consultationService.listConsultationForOwner(owner));
+    }
+
+    @GetMapping("/get-investor-consultation/{investor}")
+    public ResponseEntity listConsultationForInvestor(@PathVariable Integer investor){
+        return ResponseEntity.status(200).body(consultationService.listConsultationsForInvestor(investor));
+    }
 }
